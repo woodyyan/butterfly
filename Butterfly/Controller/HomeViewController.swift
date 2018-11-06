01deeb4f173c595375fb0cef5d237850f4a53ba8
Songbai Yan <songbai.yan@CNsongbai.yan>
2018-11-06 11:20:54 +0800

    add home page.

--- Butterfly/Controller/HomeViewController.swift
+++ Butterfly/Controller/HomeViewController.swift
@@ -13,5 +13,25 @@ class HomeViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initUI()
+    }
+    
+    private func initUI() {
+        self.title = "青娥"
+        self.tableView.backgroundColor = UIColor.background
+        self.navigationController?.navigationBar.barTintColor = UIColor.background
+        self.navigationController?.navigationBar.tintColor = UIColor.background
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.themeColor]
+        self.navigationController?.navigationBar.tintColor = UIColor.themeColor
+        
+        let rightItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(HomeViewController.pushToSettingsPage(_:)))
+        self.navigationItem.rightBarButtonItem = rightItem
+        self.navigationItem.rightBarButtonItem?.tintColor = UIColor.themeColor
+    }
+    
+    @objc func pushToSettingsPage(_ sender: UIBarButtonItem) {
+//        let settingsController = SettingsViewController()
+//        self.navigationController?.pushViewController(settingsController, animated: true)
     }
 }
---
+++ Butterfly/Extension/UIColorExtension.swift
@@ -0,0 +1,34 @@
+//
+//  UIColorExtension.swift
+//  Butterfly
+//
+//  Created by Songbai Yan  on 2018/11/6.
+//  Copyright © 2018 Songbai Yan . All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    public static var themeColor: UIColor {
+        return UIColor(red: 236, green: 106, blue: 92)!
+    }
+    
+    public static var background: UIColor {
+        return UIColor(red: 62, green: 67, blue: 72)!
+    }
+    
+    public convenience init?(red: Int, green: Int, blue: Int, transparency: CGFloat = 1) {
+        guard red >= 0 && red <= 255 else { return nil }
+        guard green >= 0 && green <= 255 else { return nil }
+        guard blue >= 0 && blue <= 255 else { return nil }
+        
+        var trans = transparency
+        if trans < 0 { trans = 0 }
+        if trans > 1 { trans = 1 }
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: trans)
+    }
+}
