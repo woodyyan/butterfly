45bfb30db20c5da222bcc29d1d05da2ba0c84fb1
Songbai Yan <songbai.yan@CNsongbai.yan>
2018-11-06 10:48:41 +0800

    add swiftlint.yml

--- Butterfly/AppDelegate.swift
+++ Butterfly/AppDelegate.swift
@@ -14,9 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    // swiftlint:disable line_length
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window!.rootViewController = UINavigationController.init(rootViewController: HomeViewController())
         return true
     }
 
---
+++ Butterfly/Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Butterfly
+//
+//  Created by Songbai Yan  on 2018/11/6.
+//  Copyright © 2018 Songbai Yan . All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
